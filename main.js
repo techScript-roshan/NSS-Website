da55e1271b5a37fbc1a471c120e93c7a6264bdc3
Roshan <rajr00267@gmail.com>
2023-12-13 00:54:20 +0530

    Bug fixed for clicking on LOAD-MORE button of 2022 & 2023 year event

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-let load_more = document.getElementById("load-more");
+let load_more2022 = document.getElementById("load-more2022");
+let load_more2023 = document.getElementById("load-more2023");
 let = document.getElementById("#logo");
 
-let currentItem_2023 = 4;
 // Loding More event by clicking on button for 2023 events
+let currentItem_2023 = 4;
 function loadMoreBtn_2023() {
   let boxes = [
     ...document.querySelectorAll(".recent_events .event_2023 .card"),
@@ -11,26 +12,28 @@ function loadMoreBtn_2023() {
     if (currentItem_2023 <= boxes.length) {
       boxes[i].style.display = "block";
     }
+    console.log(currentItem_2023);
   }
   currentItem_2023 += 2;
   // Hiding load more button after showing all the events.
   if (currentItem_2023 >= boxes.length) {
-    load_more.style.display = "none";
+    load_more2023.style.display = "none";
   }
 }
+let currentItem_2022 = 4;
 function loadMoreBtn_2022() {
   let boxes = [
     ...document.querySelectorAll(".recent_events .event_2022 .card"),
   ];
-  for (var i = currentItem_2023; i < currentItem_2023 + 2; i++) {
-    if (currentItem_2023 <= boxes.length) {
+  for (var i = currentItem_2022; i < currentItem_2022 + 2; i++) {
+    if (currentItem_2022 <= boxes.length) {
       boxes[i].style.display = "block";
     }
   }
-  currentItem_2023 += 2;
+  currentItem_2022 += 2;
   // Hiding load more button after showing all the events.
-  if (currentItem_2023 >= boxes.length) {
-    load_more.style.display = "none";
+  if (currentItem_2022 >= boxes.length) {
+    load_more2022.style.display = "none";
   }
 }
 
